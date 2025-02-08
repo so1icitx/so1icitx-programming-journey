205932bd9653bdab191f4bc187f17d1a2f2dfa3e
so1icitx <tojitiktok@proton.me>
2025-02-08 13:31:32 +0200

    art.py

---
+++ Python/Pythoncourse_1/bigger/Simple Number-Guessing-Game/art.py
@@ -0,0 +1,8 @@
+logo = """
+
+8b  8                 8                  .d88b                        w               .d88b                       
+8Ybm8 8   8 8d8b.d8b. 88b. .d88b 8d8b    8P www 8   8 .d88b d88b d88b w 8d8b. .d88    8P www .d88 8d8b.d8b. .d88b 
+8  "8 8b d8 8P Y8P Y8 8  8 8.dP' 8P      8b  d8 8b d8 8.dP' `Yb. `Yb. 8 8P Y8 8  8    8b  d8 8  8 8P Y8P Y8 8.dP' 
+8   8 `Y8P8 8   8   8 88P' `Y88P 8       `Y88P' `Y8P8 `Y88P Y88P Y88P 8 8   8 `Y88    `Y88P' `Y88 8   8   8 `Y88P 
+                                                                              wwdP                                
+"""
