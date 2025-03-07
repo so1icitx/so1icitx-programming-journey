884f84fbf16c7ed88e121ceb7ed4de1d2aa6b3a3
so1icitx <tojitiktok@proton.me>
2025-03-07 20:38:20 +0200

    Create while_ex3.py

---
+++ Python/Pythoncourse_1/simple_programs/while_ex3.py
@@ -0,0 +1,6 @@
+food = input("Enter a food you like (q to quit): ")
+while not food == "q":
+    print(f"You like {food}")
+    food = input("Enter another food you like (q to quit): ")
+
+print("Goodbye")
