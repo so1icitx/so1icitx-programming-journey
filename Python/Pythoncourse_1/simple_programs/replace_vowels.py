1e591d71b39268071bf85eb4852cf80a22ad365a
so1icitx <tojitiktok@proton.me>
2025-03-08 08:26:25 +0200

    Create replace_vowels.py

---
+++ Python/Pythoncourse_1/simple_programs/replace_vowels.py
@@ -0,0 +1,12 @@
+word = input("Enter a word: ")
+character = input("Enter a char: ")
+new_word = ""
+
+for i in word:
+    if i in "aeuio":
+        new_word += character
+    else:
+        new_word += i
+
+
+print(new_word)
