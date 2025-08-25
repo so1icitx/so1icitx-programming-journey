8f6c739a9cd0244e2e4d1a50b7cd5e5d54d14636
𝘀𝗼1𝗶𝗰𝗶𝘁𝘅 <tojitiktok@proton.me>
2025-08-25 15:16:08 +0300

    Create node1.py

---
+++ Python/MY_programs/simple/node1.py
@@ -0,0 +1,35 @@
+lass Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Our LinkedList class
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+  
+  def stringify_list(self):
+    values = []
+    while True:
+      if self.head_node == None:
+        return values
+      values.append(self.head_node.get_value())
+      
+      self.head_node = self.head_node.get_next_node()
